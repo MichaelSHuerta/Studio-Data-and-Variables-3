20a8b0b8b6d7da50ce2e81eaf9ab1eb51d798b1b
MichaelHuerta <85526782+MichaelSHuerta@users.noreply.github.com>
2021-06-11 05:04:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+// FORK this starter file and save it to your own Repl.it account.
+
+
+// Declare and initialize the 12 variables here:
+const dash = "-------------------------------------"
+
+console.log(dash)
+console.log("> LC04 - LAUNCH CHECKLIST")
+console.log(dash)
+console.log("\n")
+
+const date = "Date: Monday 2019-03-18";
+const time = "Time 10:05:34 AM";
+
+console.log(date)
+console.log(time)
+console.log("\n")
+
+console.log(dash)
+console.log("> ASTRONAUT INFO")
+console.log(dash)
+
+const count = 7;
+const status = "* status: ready";
+
+console.log("* count: "+ count )
+
+ console.log(status)
+ console.log("\n")
+
+
+ console.log(dash)
+ console.log("> FUEL DATA")
+ console.log(dash)
+
+ const fuelTempCelsius = -225
+ const fuelLevel = 100
+
+ console.log("*Fuel temp celsius: " + fuelTempCelsius + "C" )
+ console.log("*Fuel level:" + fuelLevel + "%" )
+ console.log("\n")
+
+ console.log(dash)
+ console.log("> MASS DATA")
+ console.log(dash)
+
+ const crewMass = 564.9
+ const fuelMass = 760000
+ const shuttleMass = 74842.31
+ const totalMass = 835407.21
+
+ console.log("*Crew mass: " + crewMass + "kg" )
+ console.log("*Fuel mass: " + fuelMass + "kg")
+ console.log("*Shuttle mass: " + shuttleMass + "kg")
+ console.log("*Total mass: " + totalMass + "kg")
+ console.log("\n")
+
+ console.log(dash)
+ console.log("> FLIGHT PLAN")
+ console.log(dash)
+ console.log("* weather: clear")
+ console.log("\n")
+ 
+ console.log(dash)
+ console.log("> OVERALL STATUS")
+ console.log(dash)
+ console.log("* Clear for takeoff: YES")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write code to generate the LC04 report here:
+
+
+
+
+// When done, have your TA check your code.
+
+
+
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
